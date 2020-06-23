718c439050d0587661e4bf23f7dcd2e0e09e9c63
student <student@eda-bc-05.localdomain>
2020-06-24 11:05:04 +1200

    Advice giver functional

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import request from 'superagent'
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+
+class App extends React.Component {
+
+  state = {
+    advice: ''
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    document.getElementById ('worriesTxt').value = ''
+    this.getMyAdvice ()
+  }
+
+  getMyAdvice = () => {
+    request.get("https://api.adviceslip.com/advice")
+    .then (res => {
+      this.setState({
+        advice: JSON.parse(res.text).slip.advice
+      })
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <h1>AdviceGiver.com</h1>
+        <form onSubmit={this.handleSubmit} >
+          <label> <h2>Tell me your worries </h2> </label>
+          <input type="text" id='worriesTxt'/>
+          <input type="submit" />
+        </form>
+        {this.state.advice && <p>{this.state.advice}</p>}
+      </>
+    )
+  }
+
 }
 
 export default App
